038c377c3a5bacf316c9a4efd761ababa3be98fb
bduff9 <bduff9@gmail.com>
2021-02-01 19:57:31 -0600

    Fixed unused var error

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -1,16 +1,17 @@
 import { AuthChecker } from 'type-graphql';
 
-import { TCustomContext, TUserType } from '../../api/graphql';
+import { TCustomContext /* , TUserType */ } from '../../api/graphql';
 
-export const customAuthChecker: AuthChecker<TCustomContext> = (
-	{ context },
-	roles,
-) => {
-	// const groups = context.claims?.['cognito:groups'] || [];
-	// const isAuthed = roles.some((role): boolean =>
-	// 	groups.includes(role as TUserType),
-	// );
+export const customAuthChecker: AuthChecker<TCustomContext> = () =>
+	// { context },
+	// roles,
+	// ) =>
+	{
+		// const groups = context.claims?.['cognito:groups'] || [];
+		// const isAuthed = roles.some((role): boolean =>
+		// 	groups.includes(role as TUserType),
+		// );
 
-	// return isAuthed;
-	return true;
-};
+		// return isAuthed;
+		return true;
+	};
