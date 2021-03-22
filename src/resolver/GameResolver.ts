54dba1f829b1ad97d9d9702ca95c3e6d046ddebd
bduff9 <bduff9@gmail.com>
2021-03-21 19:21:22 -0500

    Addd getCurrentWeek

--- src/resolver/GameResolver.ts
+++ src/resolver/GameResolver.ts
@@ -9,6 +9,7 @@ import {
 } from 'type-graphql';
 
 import { Game, Team } from '../entity';
+import { WEEKS_IN_SEASON } from '../util/constants';
 import { TUserType } from '../util/types';
 
 @Resolver(Game)
@@ -21,10 +22,29 @@ export class GameResolver {
 		});
 	}
 
-	@Authorized<TUserType>('user')
-	@Query(() => [Game])
-	async getGames (): Promise<Game[]> {
-		return Game.find();
+	@Authorized<TUserType>('anonymous')
+	@Query(() => Int)
+	async getCurrentWeek (): Promise<number> {
+		const { currentWeek } = await Game.createQueryBuilder('Game')
+			.select(`COALESCE(MIN(Game.GameWeek), ${WEEKS_IN_SEASON})`, 'currentWeek')
+			.where('Game.GameStatus <> :status', { status: 'C' })
+			.getRawOne<{ currentWeek: number }>();
+
+		if (currentWeek === 1) return currentWeek;
+
+		const { lastWeek, useCurrentWeek } = await Game.createQueryBuilder('Game')
+			.select('Game.GameWeek', 'lastWeek')
+			.addSelect(
+				'CURRENT_TIMESTAMP > DATE_ADD(Game.GameKickoff, INTERVAL 24 HOUR)',
+				'useCurrentWeek',
+			)
+			.where('Game.GameStatus = :status', { status: 'C' })
+			.orderBy('Game.GameKickoff', 'DESC')
+			.getRawOne<{ lastWeek: number; useCurrentWeek: '0' | '1' }>();
+
+		if (lastWeek !== currentWeek && useCurrentWeek === '0') return lastWeek;
+
+		return currentWeek;
 	}
 
 	@FieldResolver()
--- src/util/constants.ts
+++ src/util/constants.ts
@@ -13,3 +13,28 @@ export const {
 	port,
 	username,
 } = process.env;
+
+/**
+ * Number of days in a week, used for conversions
+ */
+export const DAYS_IN_WEEK = 7;
+
+/**
+ * Number of hours in a day, used for conversions
+ */
+export const HOURS_IN_DAY = 24;
+
+/**
+ * Number of minutes in an hour, used for conversions
+ */
+export const MINUTES_IN_HOUR = 60;
+
+/**
+ * Number of seconds in a minute, used for conversions
+ */
+export const SECONDS_IN_MINUTE = 60;
+
+/**
+ * The total number of weeks in an NFL regular season
+ */
+export const WEEKS_IN_SEASON = 17;
