30d3557310788bd71e3a50dd27da7e0002f0d408
bduff9 <bduff9@gmail.com>
2021-03-23 19:42:07 -0500

    Drop UserSelectedWeek from DB

---
+++ src/migration/1616544837371-DropUserSelectedWeekColumn.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class DropUserSelectedWeekColumn1616544837371
+	implements MigrationInterface {
+	public async up (queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(`alter table Users drop column UserSelectedWeek`);
+	}
+
+	public async down (queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`alter table Users add column UserSelectedWeek int not null default 1 after UserPaid`,
+		);
+	}
+}
