cec34f339c161d9fd2cb9f81d5f532821ce80528
bduff9 <bduff9@gmail.com>
2021-08-22 13:17:14 -0500

    Minor clean up for history resolver

--- src/resolver/HistoryResolver.ts
+++ src/resolver/HistoryResolver.ts
@@ -13,28 +13,16 @@
  * along with this program.  If not, see {http://www.gnu.org/licenses/}.
  * Home: https://asitewithnoname.com/
  */
-import { Arg, Authorized, FieldResolver, Int, Query, Resolver, Root } from 'type-graphql';
+import { Arg, Authorized, Int, Query, Resolver } from 'type-graphql';
 
-import { History, League, User } from '../entity';
+import { History } from '../entity';
 import { TUserType } from '../util/types';
 
 @Resolver(History)
 export class HistoryResolver {
 	@Authorized<TUserType>('user')
 	@Query(() => [History])
-	async getHistoryForYear (@Arg('Year', () => Int) year: number): Promise<History[]> {
-		return History.find({ where: { historyYear: year } });
-	}
-
-	@FieldResolver()
-	async user (@Root() history: History): Promise<User> {
-		return User.findOneOrFail({ where: { userID: history.userID } });
-	}
-
-	@FieldResolver()
-	async league (@Root() history: History): Promise<League> {
-		return League.findOneOrFail({
-			where: { leagueID: history.leagueID },
-		});
+	async getHistoryForYear (@Arg('Year', () => Int) year: number): Promise<Array<History>> {
+		return History.find({ relations: ['user'], where: { historyYear: year } });
 	}
 }
