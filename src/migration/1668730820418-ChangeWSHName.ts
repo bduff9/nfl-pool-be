ace89668925d605cf0aba07bababdc9e8266f278
bduff9 <bduff9@gmail.com>
2022-11-17 18:25:58 -0600

    Fixed WSH name

--- src/migration/1668730820418-ChangeWSHName.ts
+++ src/migration/1668730820418-ChangeWSHName.ts
@@ -16,28 +16,14 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 // ts-prune-ignore-next
-export class AddPKsToMVs1664074193905 implements MigrationInterface {
+export class ChangeWSHName1668730820418 implements MigrationInterface {
 	public async up (queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.query(
-			'ALTER TABLE OverallMV ADD COLUMN `OverallMVID` INT NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`OverallMVID`)',
-		);
-		await queryRunner.query(
-			'ALTER TABLE SurvivorMV ADD COLUMN `SurvivorMVID` INT NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`SurvivorMVID`)',
-		);
-		await queryRunner.query(
-			'ALTER TABLE WeeklyMV ADD COLUMN `WeeklyMVID` INT NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`WeeklyMVID`)',
-		);
+		await queryRunner.query(`UPDATE Teams SET TeamName = 'Commanders' WHERE TeamID = 33`);
 	}
 
 	public async down (queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.query(
-			'ALTER TABLE OverallMV DROP COLUMN `OverallMVID`, DROP PRIMARY KEY',
-		);
-		await queryRunner.query(
-			'ALTER TABLE SurvivorMV DROP COLUMN `SurvivorMVID`, DROP PRIMARY KEY',
-		);
-		await queryRunner.query(
-			'ALTER TABLE WeeklyMV DROP COLUMN `WeeklyMVID`, DROP PRIMARY KEY',
+			`UPDATE Teams SET TeamName = 'Football Team' WHERE TeamID = 33`,
 		);
 	}
 }
